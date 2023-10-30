dac7d914f855f5c209113adc910f3469cefb0240
Marco Molinari <marco.molinari@medisin.uio.no>
2023-10-30 12:02:07 +0100

    new parallel main

--- src/main_parallel.jl
+++ src/main_parallel.jl
@@ -1,4 +1,4 @@
-# main script - run simulations
+# main script - run parallel simulations
 
 using CSV
 using DataFrames
@@ -7,19 +7,19 @@ using Dates
 abs_project_path = normpath(joinpath(@__FILE__,"..", "..", "src"))
 include(joinpath(abs_project_path, "utilities", "simulation_runner.jl"))
 
-
 # Fixed parameters
 n = 800
 p = 200
 prop_non_zero_coef = 0.025
 n_replications = 2
 methods_to_evaluate=["DS", "MDS"]
-alpha_lasso = 0.7
+alpha_enet = 0.7
+use_beta_pool = true
+
 
 # Variable quantities (choose between sign strenght and beta pool vec)
-corr_coefficients_vec = [0., 0.2, 0.4, 0.6, 0.8]
+corr_coefficients_vec = [0., 0.2, 0.4, 0.5, 0.6, 0.8]
 
-use_beta_pool = true
 if use_beta_pool
     beta_signal_strength_vec = [1.]
     beta_pool = [-1., -0.8, -0.5, 0.5, 0.8, 1.]
@@ -35,18 +35,32 @@ n_combinations = length(corr_coefficients_vec) * length(beta_signal_strength_vec
 date_now = Dates.now()
 current_date = string(Dates.year(date_now)) * string(Dates.month(date_now)) * string(Dates.day(date_now))
 
-final_csv_file_name = joinpath(abs_project_path, "experiments", "results", "simulation_n_$(n)_p_$(p)_beta_$(beta_type)_ElasticNet_time_$(current_date).csv")
+# Create directory for current simulation results output
+dir_name = "n_$(n)_p_$(p)_alpha_enet_$(floor(Int, alpha_enet*10))_beta_type_$(beta_type)"
+dir_path = joinpath(abs_project_path, "results", current_date, dir_name)
+if isdir(dir_path)
+    throw("Directory already exists! Exiting execution!")
+end
+mkpath(dir_path)
+
+
+function single_csv_file_name(;base_experiment_dir, beta_value, rho_value)
+    rho_value = floor(Int, rho_value*10)
+    beta_value = floor(Int, beta_value)
+    return joinpath(
+        base_experiment_dir,
+        "beta_$(beta_value)_rho_$(rho_value).csv"
+        )
+end
 
 println("Simulation started")
 
-df_metrics_all = DataFrames.DataFrame()
-combination_counter = 0
-for corr_coeff in corr_coefficients_vec
-    for beta_signal_strength in beta_signal_strength_vec
-        global combination_counter += 1
-        println("Running combination $combination_counter of $n_combinations")
+# df_metrics_all = DataFrames.DataFrame()
 
-        # csv_file_name = "./simulation_n_$(n)_p_$(p)_rho_$(floor(Int, corr_coeff*10))_beta_$(floor(Int, beta_signal_strength))_time_$(current_date).csv"
+Threads.@threads for corr_coeff in corr_coefficients_vec
+    Threads.@threads for beta_signal_strength in beta_signal_strength_vec
+
+        csv_file_name = single_csv_file_name(base_experiment_dir=dir_path, beta_value=beta_signal_strength, rho_value=corr_coeff)
 
         data_generation_params = (
             n=n,
@@ -67,24 +81,18 @@ for corr_coeff in corr_coefficients_vec
             fdr_level=0.1,
             estimate_sigma2=true,
             methods_to_evaluate=methods_to_evaluate,
-            alpha_lasso=alpha_lasso
+            alpha_lasso=alpha_enet
         )
         
         df_metrics[!, "rho"] .= corr_coeff
         df_metrics[!, "signal_strength"] .= beta_signal_strength
 
-        # aggregate dataframes
-        if combination_counter == 1
-            global df_metrics_all = df_metrics
+        # save csv
+        CSV.write(csv_file_name, df_metrics)
 
-        elseif combination_counter > 1
-            append!(df_metrics_all, df_metrics)
-        end
     end
 end
 
 println("Simulation finished")
 
-CSV.write(final_csv_file_name, df_metrics_all)
-
-println("Results saved at $(final_csv_file_name)")
+# println("Results saved at $(common_csv_file_name)")
